3f55db6f4864092f4033bdc4c6d2aa58931dd468
eiji03aero <eiji03aero@gmail.com>
2019-11-30 00:16:55 +0900

    added test on cd

--- packages/mash/__tests__/builtins/cd.spec.ts
+++ packages/mash/__tests__/builtins/cd.spec.ts
@@ -1,16 +1,33 @@
 import { FileSystem } from 'mash-filesystem';
 
+import { ExitStatus } from '../../src/types';
 import { Environment } from '../../src/Environment';
 
 describe('builtins.cd', () => {
+  let fs: any;
+  let env: any;
+  let dir: any;
+
+  beforeEach(() => {
+    fs = FileSystem.bootstrap();
+    env = Environment.bootstrap(fs);
+    dir = fs.currentDirectory.name;
+  });
+
   it('should change directory', () => {
-    const fs = FileSystem.bootstrap();
-    const env = Environment.bootstrap(fs);
     env.eval('cd ../');
     expect(fs.currentDirectory.name).toEqual('root');
   });
 
-  it.todo('should return/exitStatus error when path to not existing directory passed');
+  it('should return/exitStatus error when path to not existing directory passed', () => {
+    env.eval('cd not_existed_dir');
+    expect(fs.currentDirectory.name).toEqual(dir);
+    expect(env.exitStatus).toEqual(ExitStatus.Failure);
+  });
 
-  it.todo('should return/exitStatus error when argument is not enough');
+  it('should return/exitStatus error when argument is not enough', () => {
+    env.eval('cd');
+    expect(fs.currentDirectory.name).toEqual(dir);
+    expect(env.exitStatus).toEqual(ExitStatus.Failure);
+  });
 });
